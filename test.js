be81077c0f582c6d27e5c437c2778c01402b681c
kehinde <fagbamigbekehinde@gmail.com>
2020-12-09 22:40:29 -0600

    Rounded Bar Chart

---
+++ test.js
@@ -0,0 +1,30 @@
+function initializeViz(){
+    var vizDiv = document.getElementById("tableauViz");
+ 
+    var vizUrl = "https://public.tableau.com/views/testingdashboardVersion2_16069950242490/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    var vizOptions = {};
+
+    viz = new tableauSoftware.Viz(vizDiv, vizUrl, vizOptions);    
+
+  };
+
+function vizFilter(filterName,filterValue,filterType){
+  //getActiveSheet() can be a dashboard or a worksheet
+  sheet=viz.getWorkbook().getActiveSheet();
+
+
+if(sheet.getSheetType() === 'worksheet'){
+sheet.applyFilterAsync(filterName,filterValue,filterType);
+}
+else{
+
+worksheetArray = sheet.getWorksheets();
+for(var i =0; i < worksheetArray.length; i++)
+{
+worksheetArray[i].applyFilterAsync(filterName,filterValue,filterType);
+console.log(worksheetArray)
+}
+}
+};
+
+

9c128223a4849418da3a540f065faac50637b507
kehinde <fagbamigbekehinde@gmail.com>
2020-12-09 22:45:28 -0600

    Tableau Public URL

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 function initializeViz(){
     var vizDiv = document.getElementById("tableauViz");
  
-    var vizUrl = "https://public.tableau.com/views/testingdashboardVersion2_16069950242490/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    //var vizUrl = "https://public.tableau.com/views/testingdashboardVersion2_16069950242490/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    var vizUrl = 'https://public.tableau.com/views/RoundedBarchart_16073912149450/RoundedBars'
     var vizOptions = {};
 
     viz = new tableauSoftware.Viz(vizDiv, vizUrl, vizOptions);    
